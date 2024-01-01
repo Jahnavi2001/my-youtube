c06839d2dc8e5e75ab186f64e1f93c1fc2e74856
vuyyuru.jahnavi <vuyyuru.jahnavi@gdn-commerce.com>
2024-01-01 07:44:37 +0530

    test

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,19 +9,18 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCvf2D-V0l_3fyI9PUBxoev9PlAynxuyvw",
-  authDomain: "my-youutube-clone.firebaseapp.com",
-  projectId: "my-youutube-clone",
-  storageBucket: "my-youutube-clone.appspot.com",
-  messagingSenderId: "704928855423",
-  appId: "1:704928855423:web:97fe3789873f327798564a",
-  measurementId: "G-0NLS8B6M72"
+	apiKey: 'AIzaSyCvf2D-V0l_3fyI9PUBxoev9PlAynxuyvw',
+	authDomain: 'my-youutube-clone.firebaseapp.com',
+	projectId: 'my-youutube-clone',
+	storageBucket: 'my-youutube-clone.appspot.com',
+	messagingSenderId: '704928855423',
+	appId: '1:704928855423:web:97fe3789873f327798564a',
+	measurementId: 'G-0NLS8B6M72',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+// const analytics = getAnalytics(app);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
